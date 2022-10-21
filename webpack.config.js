ba226d99a905d1eb57662ae7fc64c1f499c5e025
Nurlan <nergashev@fil-it.ru>
2022-10-21 16:24:15 +0700

    refactoring webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,53 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-
-let mode = 'development';
-let target = 'web';
-const production = process.env.NODE_ENV === 'production';
-
-if (production) {
-  mode = 'production';
-  target = 'browserslist';
-}
-
-const plugins = [
-  new MiniCssExtractPlugin({
-    filename: '[name].[contenthash].css',
-  }),
-  new HtmlWebpackPlugin({
-    template: './public/index.html',
-  }),
-];
-
-if (process.env.SERVE) {
-  plugins.push(new ReactRefreshWebpackPlugin());
-}
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode,
-  target,
-  plugins,
-  devtool: 'source-map',
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
+  //webpack-dev-server settings
   devServer: {
-    static: './dist',
+    contentBase: path.join(__dirname, "dist"),
     port: 3000,
-    hot: true,
   },
-
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext][query]',
-    clean: true,
-  },
-
   module: {
     rules: [
-      {test: /\.(html)$/, use: ['html-loader']},
+      /** CSS */
       {
-        test: /\.(s[ac]|c)ss$/i,
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        // npm i style-loader css-loader -D
+      },
+      /** SCSS/SAAS */
+      {
+        test: /\.s[ac]ss$/i,
         use: [
-          production ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [require('postcss-preset-env')],
-              },
-            },
-          },
-          'group-css-media-queries-loader',
-          'sass-loader',
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
         ],
+        // npm i style-loader css-loader sass sass-loader -D
       },
       {
-        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75
-              },
-            }
+        test: /\.bundle\.ts$/,
+        use: {
+          loader: 'bundle-loader',
+          options: {
+            name: '[name]'
           }
-        ],
-        type: mode === 'production' ? 'asset' : 'asset/resource',
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        enforce: "pre",
+        use: ['ts-loader',"source-map-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(svg|png|gif|jpg)$/,
+        exclude: /fonts/,
+        loader: 'file-loader'
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
@@ -97,23 +56,25 @@ module.exports = {
           filename: 'fonts/[name][ext]'
         }
       },
-      {
-        test: /\.m?[jt]sx$/, // <-- NEW
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-              "@babel/preset-typescript", // <-- NEW
-            ],
-          },
-        },
-      },
+
     ],
   },
+  /* Attempt to resolve these extensions in order.
+     If multiple files share the same name but have
+     different extensions, webpack will resolve the
+     one with the extension listed first in the
+     array and skip the rest.
+   */
   resolve: {
-    extensions: ["*", ".js", ".ts", ".tsx", ".scss"],
-  }
+    extensions: ['*','.tsx', '.ts', '.js','scss']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
